dd9ae5f83fcabfeb772eef6e2bacbf645e22f30e
RogerioAguilera <raguileraribeiro33@gmail.com>
2023-03-16 22:38:42 -0300

    feat: Adicionando gitignore

---
+++ codigo/jogo.py
@@ -0,0 +1,32 @@
+
+class Jogo:
+
+    def __init__(self,nome):
+        self.nome = nome
+
+    def __str__(self):
+        return self.nome
+    
+nbajam= Jogo('Nbajam')
+print(nbajam)
+
+topgear = Jogo('Topgear')
+print(topgear)
+
+class TipoJogo:
+      def __init__(self,tipo):
+           self.tipo = tipo
+      def __str__(self):
+           return self.tipo
+
+ação= TipoJogo('Ação')
+print(ação)
+
+aventura= TipoJogo('Aventura')
+print(aventura)
+
+estratégia= TipoJogo('Estratégia')
+print(estratégia)
+
+esporte= TipoJogo('Esporte')
+print(esporte)
---
+++ codigo/main.py
@@ -0,0 +1,8 @@
+from codigo.jogo import Jogo
+
+def teste_nome():
+    jogo_teste = Jogo('Teste')
+    print(f'Teste={jogo_teste.nome()}')
+
+teste_nome()
+
